b5481db06234be9c08dc995bdb6ff7a9cfc84066
Stephan Avenwedde <s.avenwedde@gmail.com>
2017-02-04 18:58:51 +0100

    krakenmodule

--- krakenmodule.c
+++ krakenmodule.c
@@ -1,42 +1,76 @@
 #include <Python.h>
+#include "kraken_api.h"
 
-static PyObject *spam_system(PyObject *self, PyObject *args){
+typedef struct {
+	/* makro that brings in the standard python object fields */	
+	PyObject_HEAD
+	/* Type-specific fields go here. */
+	char* sec_key;
+	char* api_key;
 
-    const char *command;
-    int sts;
+	PyObject *first;
+	PyObject *last;
 
-    if (!PyArg_ParseTuple(args, "s", &command))
-        return NULL;
-    sts = system(command);
-    return Py_BuildValue("i", sts);
+	int number;
+
+	struct kraken_api *kr_api;
+} Kraken;
+
+static void Kraken_dealloc(Kraken* self){
+
+	Py_XDECREF(self->first);
+	Py_XDECREF(self->last);
+	Py_TYPE(self)->tp_free((PyObject*)self);
 }
 
-static PyMethodDef SpamMethods[] = {
-    
-    {"system",  spam_system, METH_VARARGS,
-     "Execute a shell command."},
-    
-    {NULL, NULL, 0, NULL}        /* Sentinel */
+static PyTypeObject kraken_NoddyType = {
+	PyVarObject_HEAD_INIT(NULL, 0)
+		"kraken.Kraken",             /* tp_name */
+	sizeof(Kraken), /* tp_basicsize */
+	0,                         /* tp_itemsize */
+	0,                         /* tp_dealloc */
+	0,                         /* tp_print */
+	0,                         /* tp_getattr */
+	0,                         /* tp_setattr */
+	0,                         /* tp_reserved */
+	0,                         /* tp_repr */
+	0,                         /* tp_as_number */
+	0,                         /* tp_as_sequence */
+	0,                         /* tp_as_mapping */
+	0,                         /* tp_hash  */
+	0,                         /* tp_call */
+	0,                         /* tp_str */
+	0,                         /* tp_getattro */
+	0,                         /* tp_setattro */
+	0,                         /* tp_as_buffer */
+	Py_TPFLAGS_DEFAULT,        /* tp_flags */
+	"Kraken objects",           /* tp_doc */
 };
 
+static PyModuleDef krakenmodule = {
+	PyModuleDef_HEAD_INIT,
+	"kraken",
+	"Module that inherits Kraken API functions.",
+	-1,
+	NULL, NULL, NULL, NULL, NULL
+};
 
+	PyMODINIT_FUNC
+PyInit_kraken(void)
+{
+	PyObject* m;
 
+	kraken_NoddyType.tp_new = PyType_GenericNew;
+	if (PyType_Ready(&kraken_NoddyType) < 0)
+		return NULL;
 
-PyMODINIT_FUNC initspam(void){
+	m = PyModule_Create(&krakenmodule);
+	if (m == NULL)
+		return NULL;
 
-    (void) Py_InitModule("spam", SpamMethods);
-}
-
-int main(int argc, char *argv[]){
-
-    /* Pass argv[0] to the Python interpreter */
-    Py_SetProgramName(argv[0]);
-
-    /* Initialize the Python interpreter.  Required. */
-    Py_Initialize();
-
-    /* Add a static module */
-    initspam();
+	Py_INCREF(&kraken_NoddyType);
+	PyModule_AddObject(m, "Kraken", (PyObject *)&kraken_NoddyType);
+	return m;
 }
 
 
--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup, Extension
 
-spam = Extension('spam', sources = ['krakenmodule.c'])
+kraken = Extension('kraken', sources = ['krakenmodule.c'])
 
 setup (name = 'PackageName', version = '0.1',
         description = 'This is a demo package',
-        ext_modules = [spam])
+        ext_modules = [kraken])
 
