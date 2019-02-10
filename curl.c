4e31b008909457b761b9fd020c495a4ba7ea543c
Antti Antinoja <antti@fennosys.fi>
2019-02-10 21:01:43 +0800

    Layout fixes, curl_string -> curl_cmd_string.
    Fixed indentation.
    
    curl_get:
    * Setting pointer vars to NULL at defintion.
    * Added preallocation for "result" before entering the while loop.
    * Simplified the use of "memcount".
    * Added freeing curl_cmd_string inside the loop if realloc fails.
    * Added PERROR message if realloc fails.
    * Added free for curl_cmd_string & result for pclose error case.
    
    query_private:
    * Added checking asprintf rval test for malloc failure

--- curl.c
+++ curl.c
@@ -3,266 +3,235 @@
 #include "curl.h"
 #include "kraken_api.h"
 
+char* curl_get(char *curl_cmd_string){
 
-char* curl_get(char *query_string){
+    FILE *curl_out = NULL;
+    char *result = NULL;
+    int ext_status, i_char = 0;
+    size_t memcount = 0;
+    size_t buf_size = BUFFER_SIZE;
 
-	FILE *curl_out;
-	char *result;
-	int ext_status, i_char;
-	size_t memcount = 0;
-	result = NULL;
-	size_t buf_size = 0;
+    curl_out = popen(curl_cmd_string, "r");
 
-	curl_out = popen(query_string, "r");
-
-	do {
-		i_char = fgetc(curl_out);
-
-		memcount++;
-
-		if (memcount > buf_size) {
-			buf_size += BUFFER_SIZE;
-			char *newres = realloc(result, buf_size);
-			if (newres == NULL) {
-				free(result);
-				return NULL;	/* ERROR */
-			}
-			result = newres;
-		}
-
-		/* skip newline chars */
-		i_char == '\n' ? i_char = ' ' : i_char;  
-
-		*(result + (memcount-1)) = (char)i_char; 
-		
-
-	}while(i_char != EOF);
-
-	/* replace the EOF char with the terminate string char */
-
-	*(result + (memcount-1)) = '\0';
-
-
-	if((ext_status = pclose(curl_out)) != 0){
-		PERROR("ERROR on pclose");
-		PERROR("ERRNO-String: %s\n",strerror(errno));
+	if ( (result = malloc(buf_size)) == NULL ) {
+		PERROR("malloc failed");
+		free(curl_cmd_string);
+		return NULL;           /* return on ERROR */
 	}
 
+    while (i_char != EOF) {
+        i_char = fgetc(curl_out);
 
-	/* free memory allocated in query_private() */
+        if (memcount > buf_size) {
+            buf_size += BUFFER_SIZE;
+            char *newres = realloc(result, buf_size);
+            if (newres == NULL) {
+				free(curl_cmd_string);
+                free(result);  /* OK! */
+				PERROR("realloc failed");
+                return NULL;   /* return on ERROR */
+            }
+            result = newres;
+        }
 
-	free(query_string);
+        /* skip newline chars */
+		(i_char == '\n') ? i_char = ' ' : i_char;
 
-	return result;
+        *(result + memcount) = (char)i_char;
+        memcount++;
+    }
 
+    /* replace the EOF char with the terminate string char */
+	memcount--;
+    *(result + memcount) = '\0';
+
+    if((ext_status = pclose(curl_out)) != 0){
+        PERRORX("ERROR on pclose - errno-string: %s, curl exit status: %i\n",strerror(errno), ext_status);
+		free(curl_cmd_string);
+		free(result);
+		return NULL;
+    }
+
+    /* free memory allocated in query_private() */
+    free(curl_cmd_string);
+    return result;
 }
 
 int query_private(struct kraken_api **kr_api){
 
-	uint64_t u64_nonce;
-	int uri_length, uc_data_size;
-	unsigned char digest[32], *uc_data;
-	unsigned char *uc_b64_decoded = NULL;
-        unsigned char *uc_hmac_output = NULL;
-	char* hmac_out = NULL;
-	char* str_nonce = NULL;
-	const char* url_nonce = "nonce=";
-	char* url_seperator ="&";
-	char* s_sha256 = NULL; 
-	char* curl_query = NULL;
-	char* curl_query_url = NULL;
-	char* curl_nonce_url = NULL;
-	struct timeval sys_time;
-	struct timezone sys_tz;
+    uint64_t u64_nonce;
+    int uri_length, uc_data_size;
+    unsigned char digest[32], *uc_data;
+    unsigned char *uc_b64_decoded = NULL;
+    unsigned char *uc_hmac_output = NULL;
+    char* hmac_out = NULL;
+    char* str_nonce = NULL;
+    const char* url_nonce = "nonce=";
+    char* url_seperator ="&";
+    char* s_sha256 = NULL;
+    char* curl_cmd_string = NULL;
+    char* curl_query_url = NULL;
+    char* curl_nonce_url = NULL;
+    struct timeval sys_time;
+    struct timezone sys_tz;
 
+    if(strlen((*kr_api)->s_sec_key) != 88){
+        PDEBUG("Error: Private key must be 88 characters long");
+        return -1;
+    }
 
-	if(strlen((*kr_api)->s_sec_key) != 88){
-		PDEBUG("Error: Private key must be 88 characters long");
-		return -1;
-	}
-	/* create nonce */
+    /* create nonce */
+    sys_tz.tz_minuteswest = 0;
+    sys_tz.tz_dsttime = 0;
 
-	sys_tz.tz_minuteswest = 0;
-	sys_tz.tz_dsttime = 0;
+    gettimeofday(&sys_time, &sys_tz);
 
-	gettimeofday(&sys_time, &sys_tz);
+    u64_nonce = ((uint64_t)sys_time.tv_sec * 1000000) + sys_time.tv_usec;
 
+    /* create string from nonce */
+    if( (asprintf(&str_nonce, "%" PRIu64, u64_nonce)) < 0){
+        PDEBUG("ERROR - asprintf malloc failed");
+        return -1;
+    }
 
-	u64_nonce = ((uint64_t)sys_time.tv_sec * 1000000) + sys_time.tv_usec;
+    PTRACEX("Nonce: %s", str_nonce);
 
-	/* create string from nonce */
+    /* create nonce url with the nonce + the specific data */
+    /*   set the "nonce=" to the string */
+    curl_nonce_url = to_url(curl_nonce_url, url_nonce);
+    /*   append the nonce string to the url */
+    curl_nonce_url = to_url(curl_nonce_url, str_nonce);
+    /* example curl_nonce_url = "nonce=1234567890123456" */
 
-	asprintf(&str_nonce, "%" PRIu64, u64_nonce);
+    if(((*kr_api)->s_data)){
+        /* add seperator to the string */
+        curl_nonce_url = to_url(curl_nonce_url, url_seperator);
+        /* add the rest URI-data to the url-string */
+        curl_nonce_url = to_url(curl_nonce_url, (*kr_api)->s_data);
+    }
 
-	PTRACE("Nonce: %s", str_nonce);
+    PTRACEX("curl_nonce_url: %s", curl_nonce_url);
 
-	/*  create nonce url with the nonce + the specific data */
+    /* CRYPTO FUNCTIONS */
+    /*   s_sha256 = "1234567890123456nonce=12332.....pair=XBT.."  */
+    s_sha256 = to_url(s_sha256, str_nonce);
+    s_sha256 = to_url(s_sha256, curl_nonce_url);
+    PTRACEX("s_sha256 nonce: %s", s_sha256);
 
-	/* set the "nonce=" to the string */
-	curl_nonce_url = to_url(curl_nonce_url, url_nonce);
-	/* append the nonce string to the url */
-	curl_nonce_url = to_url(curl_nonce_url, str_nonce);
+    /* execute SHA256 hash-algorithm */
+    if((sha256(s_sha256, digest)) != 0){
+        PDEBUG("Error calling SHA256");
+        return -1;
+    }
 
-	/* e.g. curl_nonce_url = "nonce=1234567890123456" */
+    uri_length = (int)strlen((*kr_api)->tmp_query_url);
+    PTRACEX("uri_path: %s", (*kr_api)->tmp_query_url);
 
-	if(((*kr_api)->s_data)){
+    uc_data_size = uri_length + SHA256_DIGEST_LENGTH;
 
-		/* add seperator to the string */
+    /* MALLOC: posix functions cannot be used;
+     * a trailing '\0' must not exist */
+    uc_data = malloc(uc_data_size);
 
-		curl_nonce_url = to_url(curl_nonce_url, url_seperator);
+    memcpy(uc_data, (*kr_api)->tmp_query_url, uri_length);
+    memcpy(uc_data+uri_length, digest, SHA256_DIGEST_LENGTH);
 
-		/* add the rest URI-data to the url-string */
+    /* BASE64 decoding */
+    uc_b64_decoded = base64_decode((*kr_api)->s_sec_key, uc_b64_decoded);
 
-		curl_nonce_url = to_url(curl_nonce_url, (*kr_api)->s_data);
-	}
+    /* HMAC decoding */
+    uc_hmac_output = hmac_sha512(uc_data, uc_b64_decoded, uc_hmac_output, uc_data_size);
 
-	PTRACE("curl_nonce_url: %s", curl_nonce_url);
+    hmac_out = base64_encode(uc_hmac_output);
 
+    PTRACEX("HMAC out: %s", hmac_out);
 
-	/*  CRYPTO FUNCTIONS */
-	
+    /* create the curl query string:
+     * complete tmp_query_url */
+    curl_query_url = strdup((*kr_api)->s_url);
+    curl_query_url = to_url(curl_query_url, (*kr_api)->tmp_query_url);
 
-	/* s_sha256 = "1234567890123456nonce=12332.....pair=XBT.."  */
+    /* GENERATE THE CURL COMMAND LINE ARGUMENT STRING */
+    /* -tlsv1.3 to force TLS V1.3 support */
+    curl_cmd_string = strdup("curl -sS -tlsv1.3 --data ");
+    curl_cmd_string = to_url(curl_cmd_string, "\"");
+    curl_cmd_string = to_url(curl_cmd_string, curl_nonce_url);
+    curl_cmd_string = to_url(curl_cmd_string, "\" ");
+    curl_cmd_string = to_url(curl_cmd_string, "--header ");
+    curl_cmd_string = to_url(curl_cmd_string, "\"");
+    curl_cmd_string = to_url(curl_cmd_string, "API-Key: ");
+    curl_cmd_string = to_url(curl_cmd_string, (*kr_api)->s_api_key);
+    curl_cmd_string = to_url(curl_cmd_string, "\" ");
+    curl_cmd_string = to_url(curl_cmd_string, "--header ");
+    curl_cmd_string = to_url(curl_cmd_string, "\"");
+    curl_cmd_string = to_url(curl_cmd_string, "API-Sign: ");
+    curl_cmd_string = to_url(curl_cmd_string, hmac_out);
+    curl_cmd_string = to_url(curl_cmd_string, "\" ");
+    curl_cmd_string = to_url(curl_cmd_string, curl_query_url);
 
-	s_sha256 = to_url(s_sha256, str_nonce);
-	s_sha256 = to_url(s_sha256, curl_nonce_url);
+    /* free s_data only if it was used */
+    if((*kr_api)->s_data){
+        free((*kr_api)->s_data);
+        (*kr_api)->s_data = NULL;
+    }
 
-	PTRACE("s_sha256 nonce: %s", s_sha256);
+    free(curl_query_url);
+    free(curl_nonce_url);
+    free((*kr_api)->tmp_query_url);
+    (*kr_api)->tmp_query_url = NULL;
+    free(str_nonce);
+    free(s_sha256);
+    free(uc_data);
+    free(uc_b64_decoded);
+    free(hmac_out);
+    free(uc_hmac_output);
 
-	/* execute SHA256 hash-algorithm */
+    PTRACEX("\n%s\n", curl_cmd_string);
+    (*kr_api)->s_result = curl_get(curl_cmd_string);
 
-	if((sha256(s_sha256, digest)) != 0){
-		PDEBUG("Error calling SHA256"); 
-		return -1;
-	}
-
-	uri_length = (int)strlen((*kr_api)->tmp_query_url);
-
-	PTRACE("uri_path: %s", (*kr_api)->tmp_query_url);
-	uc_data_size =uri_length + SHA256_DIGEST_LENGTH;
-
-
-	/* MALLOC: posix functions cannot be used;
-	 * a trailing '\0' must not exist */
-
-	uc_data = malloc(uc_data_size);
-
-	memcpy(uc_data, (*kr_api)->tmp_query_url, uri_length);
-	memcpy(uc_data+uri_length, digest, SHA256_DIGEST_LENGTH);
-
-
-	/* BASE64 decoding */
-
-
-	uc_b64_decoded = base64_decode((*kr_api)->s_sec_key, uc_b64_decoded);
-
-	/* HMAC decoding */
-
-	uc_hmac_output = hmac_sha512(uc_data, uc_b64_decoded, uc_hmac_output, uc_data_size);
-
-	hmac_out = base64_encode(uc_hmac_output);
-
-	PTRACE("HMAC out: %s", hmac_out);
-
-	/* create the curl query string:
-	 * complete tmp_query_url */
-
-	curl_query_url = strdup((*kr_api)->s_url);
-	curl_query_url = to_url(curl_query_url, (*kr_api)->tmp_query_url);
-
-	/* GENERATE THE CURL COMMAND LINE ARGUMENT STRING */
-
-	/* -tlsv1.3 to force TLS V1.3 support */
-
-	curl_query = strdup("curl -sS -tlsv1.3 --data ");
-	curl_query = to_url(curl_query, "\"");
-	curl_query = to_url(curl_query, curl_nonce_url);
-	curl_query = to_url(curl_query, "\" ");
-	curl_query = to_url(curl_query, "--header ");
-	curl_query = to_url(curl_query, "\"");
-	curl_query = to_url(curl_query, "API-Key: ");
-	curl_query = to_url(curl_query, (*kr_api)->s_api_key);
-	curl_query = to_url(curl_query, "\" ");
-	curl_query = to_url(curl_query, "--header ");
-	curl_query = to_url(curl_query, "\"");
-	curl_query = to_url(curl_query, "API-Sign: ");
-	curl_query = to_url(curl_query, hmac_out);
-	curl_query = to_url(curl_query, "\" ");
-	curl_query = to_url(curl_query, curl_query_url);
-
-
-
-	/* free s_data only if it was used */
-	if((*kr_api)->s_data){
-		free((*kr_api)->s_data);
-		(*kr_api)->s_data = NULL;
-	}
-
-	free(curl_query_url);
-	free(curl_nonce_url);
-	free((*kr_api)->tmp_query_url);
-	(*kr_api)->tmp_query_url = NULL;
-	free(str_nonce);
-	free(s_sha256);
-	free(uc_data);
-	free(uc_b64_decoded);
-	free(hmac_out);
-	free(uc_hmac_output);
-
-	PTRACE("\n%s\n", curl_query);
-	
-	(*kr_api)->s_result = curl_get(curl_query);
-
-	return 0;
+    return 0;
 }
 
-
 int query_public(struct kraken_api **kr_api){
+    char* curl_query_url = NULL;
+    char* curl_cmd_string = NULL;
 
+    /* create the curl query string:
+     * complete tmp_query_url */
+    curl_query_url = strdup((*kr_api)->s_url);
+    curl_query_url = to_url(curl_query_url, (*kr_api)->tmp_query_url);
 
-	char* curl_query_url	= NULL;
-	char* curl_query	= NULL;
+    /* GENERATE THE CURL COMMAND LINE ARGUMENT STRING */
+    /* -tlsv1.3 to force TLS V1.3 support */
+    curl_cmd_string = strdup("curl -sS -tlsv1.3 ");
 
-	/* create the curl query string:
-	 * complete tmp_query_url */
+    PTRACEX("curl query: %s", (*kr_api)->s_data);
 
-	curl_query_url = strdup((*kr_api)->s_url);
-	curl_query_url = to_url(curl_query_url, (*kr_api)->tmp_query_url);
+    if((*kr_api)->s_data){
+        curl_cmd_string = to_url(curl_cmd_string, "--data ");
+        curl_cmd_string = to_url(curl_cmd_string, "\"");
+        curl_cmd_string = to_url(curl_cmd_string, (*kr_api)->s_data);
+        curl_cmd_string = to_url(curl_cmd_string, "\" ");
+    }
 
-	/* GENERATE THE CURL COMMAND LINE ARGUMENT STRING */
+    curl_cmd_string = to_url(curl_cmd_string, "\"");
+    curl_cmd_string = to_url(curl_cmd_string, curl_query_url);
+    curl_cmd_string = to_url(curl_cmd_string, "\"");
 
-	/* -tlsv1.3 to force TLS V1.3 support */
+    PTRACEX("curl query: %s", curl_cmd_string);
 
-	curl_query = strdup("curl -sS -tlsv1.3 ");
+    /* free s_data only if it was used */
+    if((*kr_api)->s_data){
+        free((*kr_api)->s_data);
+        (*kr_api)->s_data = NULL;
+    }
 
+    free((*kr_api)->tmp_query_url);
+    (*kr_api)->tmp_query_url = NULL;
 
-	PTRACE("curl query: %s", (*kr_api)->s_data);
+    free(curl_query_url);
 
-	if((*kr_api)->s_data){
-		curl_query = to_url(curl_query, "--data ");
-		curl_query = to_url(curl_query, "\"");
-	        curl_query = to_url(curl_query, (*kr_api)->s_data);	
-		curl_query = to_url(curl_query, "\" ");
-	}
+    (*kr_api)->s_result = curl_get(curl_cmd_string);
 
-	curl_query = to_url(curl_query, "\"");
-	curl_query = to_url(curl_query, curl_query_url);
-	curl_query = to_url(curl_query, "\"");
-
-	PTRACE("curl query: %s", curl_query);
-
-	/* free s_data only if it was used */
-	if((*kr_api)->s_data){
-		free((*kr_api)->s_data);
-		(*kr_api)->s_data = NULL;
-	}
-
-	free((*kr_api)->tmp_query_url);
-	(*kr_api)->tmp_query_url = NULL;
-
-	free(curl_query_url);
-
-	(*kr_api)->s_result = curl_get(curl_query);
-
-	return 0;
+    return 0;
 }
