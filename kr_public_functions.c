5362b79eea4d94faa9df00791a65831d0d912acd
Antti Antinoja <antti@fennosys.fi>
2019-02-10 21:57:54 +0800

    Removed u8_opt_count, layout fixes.
    P-macro (PTRACE, PTRACEX etc.) updates.

--- kr_public_functions.c
+++ kr_public_functions.c
@@ -1,15 +1,12 @@
 #include "kr_public_functions.h"
 
-extern uint8_t u8_opt_count;
 
 int server_time(struct kraken_api **kr_api){
-
 
 	(*kr_api)->tmp_query_url = to_url((*kr_api)->tmp_query_url, (*kr_api)->s_uri_public);	
 	(*kr_api)->tmp_query_url = to_url((*kr_api)->tmp_query_url, (*kr_api)->s_uri_server_time);
 
-	PTRACE("server time executed: %s", (*kr_api)->tmp_query_url);
-
+	PTRACEX("server time executed: %s", (*kr_api)->tmp_query_url);
 	query_public(kr_api);
 
 	return 0;
@@ -18,7 +15,6 @@ int server_time(struct kraken_api **kr_api){
 int asset_info(struct kraken_api **kr_api){
 
 	/* set the appropriate optionals for this function */
-
 	(*kr_api)->opt_table[INFO].b_flag	= TRUE;
 	(*kr_api)->opt_table[ACLASS].b_flag	= TRUE;
 	(*kr_api)->opt_table[ASSET].b_flag	= TRUE;
@@ -26,16 +22,12 @@ int asset_info(struct kraken_api **kr_api){
 	(*kr_api)->tmp_query_url = to_url((*kr_api)->tmp_query_url, (*kr_api)->s_uri_public);	
 	(*kr_api)->tmp_query_url = to_url((*kr_api)->tmp_query_url, (*kr_api)->s_uri_asset_info);
 	
-
 	/* set the recursive counter to zero */
 
-	u8_opt_count = 0;
-
 	/* check if optionals are given */	
-
 	switch_opt(kr_api);
 
-	PTRACE("(*kr_api)->s_data = %s", (*kr_api)->s_data);
+	PTRACEX("(*kr_api)->s_data = %s", (*kr_api)->s_data);
 	query_public(kr_api);
 
 	return 0;
@@ -44,23 +36,18 @@ int asset_info(struct kraken_api **kr_api){
 int asset_pairs(struct kraken_api **kr_api){
 
 	/* set the appropriate optionals for this function */
-
 	(*kr_api)->opt_table[INFO].b_flag	= TRUE;
 	(*kr_api)->opt_table[PAIR].b_flag	= TRUE;
 
 	(*kr_api)->tmp_query_url = to_url((*kr_api)->tmp_query_url, (*kr_api)->s_uri_public);	
 	(*kr_api)->tmp_query_url = to_url((*kr_api)->tmp_query_url, (*kr_api)->s_uri_asset_pairs);
 	
-
 	/* set the recursive counter to zero */
 
-	u8_opt_count = 0;
-
 	/* check if optionals are given */	
-
 	switch_opt(kr_api);
 
-	PTRACE("(*kr_api)->s_data = %s", (*kr_api)->s_data);
+	PTRACEX("(*kr_api)->s_data = %s", (*kr_api)->s_data);
 	query_public(kr_api);
 
 	return 0;
@@ -73,17 +60,12 @@ int ticker_info(struct kraken_api **kr_api, const char *pair){
 	(*kr_api)->tmp_query_url = to_url((*kr_api)->tmp_query_url, (*kr_api)->s_uri_public);
 	(*kr_api)->tmp_query_url = to_url((*kr_api)->tmp_query_url, (*kr_api)->s_uri_ticker);
 
-
 	(*kr_api)->s_data = to_url((*kr_api)->s_data, url_pair);
 	(*kr_api)->s_data = to_url((*kr_api)->s_data, pair);
 
 	query_public(kr_api);
 
-
-
-
 	return 0;
-
 }
 
 
@@ -91,10 +73,8 @@ int ohlc_data(struct kraken_api **kr_api, const char *pair){
 
 	const char* url_pair = "pair=";
 
-	
 	(*kr_api)->opt_table[INTERVAL].b_flag	= TRUE;
 	(*kr_api)->opt_table[SINCE].b_flag	= TRUE;
-
 
 	(*kr_api)->tmp_query_url = to_url((*kr_api)->tmp_query_url, (*kr_api)->s_uri_public);
 	(*kr_api)->tmp_query_url = to_url((*kr_api)->tmp_query_url, (*kr_api)->s_uri_ohlc);
@@ -104,17 +84,13 @@ int ohlc_data(struct kraken_api **kr_api, const char *pair){
 
 	/* set the recursive counter to zero */
 
-	u8_opt_count = 0;
-
 	/* check if optionals are given */	
-
 	switch_opt(kr_api);
 
-	PTRACE("(*kr_api)->s_data = %s", (*kr_api)->s_data);
+	PTRACEX("(*kr_api)->s_data = %s", (*kr_api)->s_data);
 	query_public(kr_api);
 
 	return 0;
-
 }
 
 
@@ -127,21 +103,16 @@ int order_book(struct kraken_api **kr_api, const char *pair){
 	(*kr_api)->tmp_query_url = to_url((*kr_api)->tmp_query_url, (*kr_api)->s_uri_public);
 	(*kr_api)->tmp_query_url = to_url((*kr_api)->tmp_query_url, (*kr_api)->s_uri_order_book);
 
-
 	(*kr_api)->s_data = to_url((*kr_api)->s_data, url_pair);
 	(*kr_api)->s_data = to_url((*kr_api)->s_data, pair);
 
 	/* set the recursive counter to zero */
 
-	u8_opt_count = 0;
-
 	/* check if optionals are given */	
-
 	switch_opt(kr_api);
 
-	PTRACE("(*kr_api)->s_data = %s", (*kr_api)->s_data);
+	PTRACEX("(*kr_api)->s_data = %s", (*kr_api)->s_data);
 	query_public(kr_api);
-
 
 	return 0;
 }
@@ -156,21 +127,16 @@ int recent_trades(struct kraken_api **kr_api, const char *pair){
 	(*kr_api)->tmp_query_url = to_url((*kr_api)->tmp_query_url, (*kr_api)->s_uri_public);
 	(*kr_api)->tmp_query_url = to_url((*kr_api)->tmp_query_url, (*kr_api)->s_uri_recent_trades);
 
-
 	(*kr_api)->s_data = to_url((*kr_api)->s_data, url_pair);
 	(*kr_api)->s_data = to_url((*kr_api)->s_data, pair);
 
 	/* set the recursive counter to zero */
 
-	u8_opt_count = 0;
-
 	/* check if optionals are given */	
-
 	switch_opt(kr_api);
 
-	PTRACE("(*kr_api)->s_data = %s", (*kr_api)->s_data);
+	PTRACEX("(*kr_api)->s_data = %s", (*kr_api)->s_data);
 	query_public(kr_api);
-
 
 	return 0;
 }
@@ -185,21 +151,16 @@ int spread_data(struct kraken_api **kr_api, const char *pair){
 	(*kr_api)->tmp_query_url = to_url((*kr_api)->tmp_query_url, (*kr_api)->s_uri_public);
 	(*kr_api)->tmp_query_url = to_url((*kr_api)->tmp_query_url, (*kr_api)->s_uri_spread);
 
-
 	(*kr_api)->s_data = to_url((*kr_api)->s_data, url_pair);
 	(*kr_api)->s_data = to_url((*kr_api)->s_data, pair);
 
 	/* set the recursive counter to zero */
 
-	u8_opt_count = 0;
-
 	/* check if optionals are given */	
-
 	switch_opt(kr_api);
 
-	PTRACE("(*kr_api)->s_data = %s", (*kr_api)->s_data);
+	PTRACEX("(*kr_api)->s_data = %s", (*kr_api)->s_data);
 	query_public(kr_api);
-
 
 	return 0;
 }
