hANSIc99/Kraken_C_API

commit e38aca44ac798e093e18694192dce86f75bad988

Removed recursion, added check reset for val only.
I think loop is more clear and doesn't require resetting counter on
caller functions.

val only situation is an error sitate presented by library user by
applying an unused option for the function. Added PTRACEX note for this
case. PERROR would be in order?

kr_helper.c
@@ -1,47 +1,46 @@
 #include "kr_helper.h"
 
-
-uint8_t u8_opt_count = 0; 
-
 void switch_opt(struct kraken_api **kr_api){
 
-	const char* url_seperator = "&";
-	uint8_t b_flag;
-	char *value;
+	const char url_seperator = '&';
+	uint8_t u8_opt_count = (*kr_api)->opt_table_lenght ;
 
+	while (u8_opt_count--) {
+		/* b_flag is set by the handler function and it defines if this opt
+		 * is allowed in the scope of the current function */
+		uint8_t cur_b_flag = (*kr_api)->opt_table[u8_opt_count].b_flag;
+		/* .val is set by kraken_set_opt (called from the user application) */
+		char *cur_val = (*kr_api)->opt_table[u8_opt_count].val;
 
-	b_flag	= (*kr_api)->opt_table[u8_opt_count].b_flag;
-	value	= (*kr_api)->opt_table[u8_opt_count].val;
-
-	/* if an option was set by the previous function */
-	if(b_flag && value){
-
-		/* check if the string is not empty */
-		if((*kr_api)->s_data)
-			/* place a "&" behind the excisting data */
-			(*kr_api)->s_data = to_url((*kr_api)->s_data, url_seperator);	
-
-
-		/* concatenate url with "KEY" + "VALUE" + "&" */
-		(*kr_api)->s_data = to_url((*kr_api)->s_data, (*kr_api)->opt_table[u8_opt_count].key);
-		(*kr_api)->s_data = to_url((*kr_api)->s_data, value);
-		/* set the flag to FALSE again in case of subsequent calls */
-		(*kr_api)->opt_table[u8_opt_count].b_flag = FALSE;
-		/* free the value stored in the array (in case of subsequent calls) */
-		free((*kr_api)->opt_table[u8_opt_count].val);
-		(*kr_api)->opt_table[u8_opt_count].val = NULL;
-	}else if(b_flag){
-		/* if no data was found in "VALUE", just set the falg back to FALSE */
-		(*kr_api)->opt_table[u8_opt_count].b_flag = FALSE;
+		if( cur_b_flag && (cur_val != NULL) ){
+			/* GO: let's construct s_data: */
+			if( (*kr_api)->s_data != NULL )
+				/* we have multiple arguments so we'll append an '&' to the existing string : */
+				(*kr_api)->s_data = to_url((*kr_api)->s_data, &url_seperator);
+			/* concatenate url with "KEY" + "VALUE" + "&" */
+			(*kr_api)->s_data = to_url((*kr_api)->s_data, (*kr_api)->opt_table[u8_opt_count].key);
+			(*kr_api)->s_data = to_url((*kr_api)->s_data, cur_val);
+			PTRACEX("u8_count: %d, cur_val: %s", u8_opt_count, cur_val);
+			/* prepare things for subsequent calls: */
+			(*kr_api)->opt_table[u8_opt_count].b_flag = FALSE;
+			free((*kr_api)->opt_table[u8_opt_count].val);
+			(*kr_api)->opt_table[u8_opt_count].val = NULL;
+		}
+		else if(cur_b_flag) {
+			/* only b_flaf present. reset b_flag so we are ready for new calls */
+			(*kr_api)->opt_table[u8_opt_count].b_flag = FALSE;
+		}
+		else if(cur_val != NULL) {
+			/* only cur_val present. free & reset val so we are ready for new calls. 
+			 * we need to have this since the library user could erroneusly set an option which
+			 * is not accepted by the current function thus making the option to appear in an
+			 * sunsequent call to a function which allows it.
+			 * (This is actually an error state?) */
+			PTRACEX("NOTE: Option \"%s\" not accepted with this function",
+					(*kr_api)->opt_table[u8_opt_count].name);
+			free((*kr_api)->opt_table[u8_opt_count].val);
+			(*kr_api)->opt_table[u8_opt_count].val = NULL;
+		}
 	}
-
-	/* inkrement the global counter */
-	u8_opt_count++ ;
-
-	PTRACE("u8_count: %d ; (*kr_api)->s_data: %s", u8_opt_count, (*kr_api)->s_data);
-
-	/* check if all option were tested; if yes: RECURSIVE call */
-	if(u8_opt_count < ((*kr_api)->opt_table_lenght))
-		switch_opt(kr_api);	
-	
+	PTRACEX( "(*kr_api)->s_data = %s", (*kr_api)->s_data );
 }

commit 1cdcec5b9b8fc925a83ee4d3b33b8050d74e654a

Fix unbound read - regression introduced by e38aca4

kr_helper.c
@@ -2,7 +2,7 @@
 
 void switch_opt(struct kraken_api **kr_api){
 
-	const char url_seperator = '&';
+	const char* url_seperator = "&";
 	uint8_t u8_opt_count = (*kr_api)->opt_table_lenght ;
 
 	while (u8_opt_count--) {
@@ -16,7 +16,7 @@ void switch_opt(struct kraken_api **kr_api){
 			/* GO: let's construct s_data: */
 			if( (*kr_api)->s_data != NULL )
 				/* we have multiple arguments so we'll append an '&' to the existing string : */
-				(*kr_api)->s_data = to_url((*kr_api)->s_data, &url_seperator);
+				(*kr_api)->s_data = to_url((*kr_api)->s_data, url_seperator);
 			/* concatenate url with "KEY" + "VALUE" + "&" */
 			(*kr_api)->s_data = to_url((*kr_api)->s_data, (*kr_api)->opt_table[u8_opt_count].key);
 			(*kr_api)->s_data = to_url((*kr_api)->s_data, cur_val);
@@ -27,14 +27,14 @@ void switch_opt(struct kraken_api **kr_api){
 			(*kr_api)->opt_table[u8_opt_count].val = NULL;
 		}
 		else if(cur_b_flag) {
-			/* only b_flaf present. reset b_flag so we are ready for new calls */
+			/* only b_flag present. reset b_flag so we are ready for new calls */
 			(*kr_api)->opt_table[u8_opt_count].b_flag = FALSE;
 		}
 		else if(cur_val != NULL) {
 			/* only cur_val present. free & reset val so we are ready for new calls.
-			 * we need to have this since the library user could erroneusly set an option which
+			 * we need to have this since the library user could erroneously set an option which
 			 * is not accepted by the current function thus making the option to appear in an
-			 * sunsequent call to a function which allows it.
+			 * subsequent call to a function which allows it.
 			 * (This is actually an error state?) */
 			PTRACEX("NOTE: Option \"%s\" not accepted with this function",
 					(*kr_api)->opt_table[u8_opt_count].name);