hANSIc99/Kraken_C_API

commit ff01060eb29072266251c217d4486f22a1be0846

Fix buffer overflow in curl_get()

curl.c
@@ -9,21 +9,26 @@ char* curl_get(char *query_string){
 	FILE *curl_out;
 	char *result;
 	int ext_status, i_char;
-	int memcount = 0;
+	size_t memcount = 0;
 	result = NULL;
+	size_t buf_size = 0;
 
-
-	result = calloc(BUFFER_SIZE, sizeof(char));
 	curl_out = popen(query_string, "r");
-
-	if(curl_out == NULL)
-
-		return NULL;	/* ERROR */
 
 	do {
 		i_char = fgetc(curl_out);
 
 		memcount++;
+
+		if (memcount > buf_size) {
+			buf_size += BUFFER_SIZE;
+			char *newres = realloc(result, buf_size);
+			if (newres == NULL) {
+				free(result);
+				return NULL;	/* ERROR */
+			}
+			result = newres;
+		}
 
 		/* skip newline chars */
 		i_char == '\n' ? i_char = ' ' : i_char;  

commit 183a92d0092d17658511a0f079a66bf230a0f878

Check curl_out for NULL - regression introduced by ff01060

curl.c
@@ -12,6 +12,10 @@ char* curl_get(char *curl_cmd_string){
 	size_t buf_size = BUFFER_SIZE;
 
 	curl_out = popen(curl_cmd_string, "r");
+	if(curl_out == NULL) {
+		free(curl_cmd_string);
+		return NULL;    /* ERROR */
+	}
 
 	if ( (result = malloc(buf_size)) == NULL ) {
 		PERROR("malloc failed");